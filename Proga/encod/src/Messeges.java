024e3e6163dfa25219f89a520a35d7da1c3e2720
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-09 22:01:17 +0300

    Delete Proga/encod directory

--- Proga/encod/src/Messeges.java
+++
@@ -1,4 +0,0 @@
-public class Messeges {
-    static final char[] use = {1080, 1089, 1087, 1086, 1083, 1100, 1079, 1091, 1077, 1090};
-    static final char[] vafla = {1074, 1072, 1092, 1083, 1103};
-}
