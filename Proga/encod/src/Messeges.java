1693a8057df6d6b1b224cdda19fb2e5f5b3bd546
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-09 21:59:27 +0300

    Add files via upload

---
+++ Proga/encod/src/Messeges.java
@@ -0,0 +1,4 @@
+public class Messeges {
+    static final char[] use = {1080, 1089, 1087, 1086, 1083, 1100, 1079, 1091, 1077, 1090};
+    static final char[] vafla = {1074, 1072, 1092, 1083, 1103};
+}
---
+++ Proga/encod/src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+
+        System.out.println(new String(Messeges.vafla) + " " + new String(Messeges.use) + " Dragon Breath");
+        double a = 1.5;
+
+    }
+}
